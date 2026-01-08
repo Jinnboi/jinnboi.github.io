c8f16605473737fae66c308eaf837537f9ad30f4
Jinnboi <marcosmancia77@gmail.com>
2026-01-08 14:46:50 -0800

    Revamped website to include more JS and simpler palette. Inspired by my new laptop setup.

---
+++ script.js
@@ -0,0 +1,29 @@
+/* ######## Smooth Scroll ######## */
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+/* ######## Fade In & Out on Scroll ######## */
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.15
+  }
+);
+
+document.querySelectorAll(".fade-in").forEach(el => {
+  observer.observe(el);
+});
+
